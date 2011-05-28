df85c37e9633acc7e6e901bcf3dd04b8f5b6d777
flowerrrr <okarito2000@yahoo.de>
2011-05-29 00:31:44 +0200

    test commit

---
+++ rmt-manager/src/main/java/de/flower/rmt/ui.manager/FooBar.java
@@ -0,0 +1,11 @@
+package de.flower.rmt.ui.manager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: oblume
+ * Date: 29.05.11
+ * Time: 00:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class FooBar {
+}
