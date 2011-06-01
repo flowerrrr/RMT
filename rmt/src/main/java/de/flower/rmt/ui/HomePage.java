086415232c7f2b0d9f5484d392c1e7ad80d4c200
flowerrrr <okarito2000@yahoo.de>
2011-06-02 00:01:56 +0200

    logout link

--- rmt/src/main/java/de/flower/rmt/ui/HomePage.java
+++ rmt/src/main/java/de/flower/rmt/ui/HomePage.java
@@ -4,7 +4,9 @@ import de.flower.rmt.dao.ITestDao;
 import de.flower.rmt.model.TestBE;
 import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.component.IRequestablePage;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
@@ -25,6 +27,25 @@ public class HomePage extends BasePage {
 
         add(new Label("testBE", Model.of(testBE.toString())));
 
+        add(new LogoutLink("logoutLink", this.getClass()));
+
+    }
+
+    private static class LogoutLink extends Link {
+
+        private Class<? extends IRequestablePage> responsePage;
+
+        public LogoutLink(String id, Class<? extends IRequestablePage> responsePage) {
+            super(id);
+            this.responsePage = responsePage;
+        }
+
+        @Override
+        public void onClick() {
+            org.apache.wicket.authentication.AuthenticatedWebSession session = WebSession.get();
+            session.signOut();
+            setResponsePage(responsePage);
+        }
     }
 
 }
