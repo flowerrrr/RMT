e023e88dee5ce6f28a9c43cd86cebf604a49e5eb
flowerrrr <okarito2000@yahoo.de>
2011-06-13 13:09:14 +0200

    attempt to implement Hibernate filter. stopped it, but want to keep code for later usage.

--- rmt/src/main/java/de/flower/rmt/repository/interceptor/FilterEnabler.java
+++ rmt/src/main/java/de/flower/rmt/repository/interceptor/FilterEnabler.java
@@ -1,6 +1,8 @@
 package de.flower.rmt.repository.interceptor;
 
+import de.flower.common.util.SecurityContextWrapper;
 import de.flower.common.util.logging.Slf4jUtil;
+import org.apache.wicket.Session;
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.Transaction;
 import org.slf4j.Logger;
@@ -12,11 +14,13 @@ public class FilterEnabler extends EmptyInterceptor {
 
     private final static Logger log = Slf4jUtil.getLogger();
 
-
     @Override
     public void afterTransactionBegin(Transaction tx) {
 
         // need Session and current Club-Id
-        log.info("§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§ Interceptor called ");
+        SecurityContextWrapper.getUserFromSecurityContext();
+
+        Session.get().set
     }
+
 }
