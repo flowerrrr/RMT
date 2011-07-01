b24df53efe0dff0069d28bfaffe5f10ff904fd63
flowerrrr <okarito2000@yahoo.de>
2011-07-02 00:33:37 +0200

    added veil to disable double clicks

--- rmt/src/main/java/de/flower/rmt/ui/app/AbstractBasePage.java
+++ rmt/src/main/java/de/flower/rmt/ui/app/AbstractBasePage.java
@@ -1,12 +1,17 @@
 package de.flower.rmt.ui.app;
 
+import org.apache.wicket.ajax.IAjaxIndicatorAware;
 import org.apache.wicket.markup.html.WebPage;
 
 /**
  * @author oblume
  */
 @Deprecated // might be obsolete
-public class AbstractBasePage extends WebPage {
+public class AbstractBasePage extends WebPage implements IAjaxIndicatorAware {
 
+    @Override
+    public String getAjaxIndicatorMarkupId() {
+        return "veil";
+    }
 
 }
--- rmt/src/main/java/de/flower/rmt/ui/common/page/BasePage.java
+++ rmt/src/main/java/de/flower/rmt/ui/common/page/BasePage.java
@@ -1,10 +1,11 @@
 package de.flower.rmt.ui.common.page;
 
 import de.flower.rmt.ui.app.AbstractBasePage;
+import org.apache.wicket.ajax.IAjaxIndicatorAware;
 
 /**
  * @author oblume
  */
-public class BasePage extends AbstractBasePage {
+public class BasePage extends AbstractBasePage  {
 
 }
