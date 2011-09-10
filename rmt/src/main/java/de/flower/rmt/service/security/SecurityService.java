25cf71bf1089081c0c5a9a69264cb1113e4c4eeb
flowerrrr <okarito2000@yahoo.de>
2011-09-10 22:33:43 +0200

    code cleaning

--- rmt/src/main/java/de/flower/rmt/service/security/SecurityService.java
+++ rmt/src/main/java/de/flower/rmt/service/security/SecurityService.java
@@ -1,26 +1,18 @@
 package de.flower.rmt.service.security;
 
 import de.flower.rmt.model.Users;
-import de.flower.rmt.service.IUserManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author oblume
  */
 @Service
-@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 public class SecurityService implements ISecurityService {
 
     @Autowired
     private SecurityContextHolderStrategy schs;
-
-    @Autowired
-    private IUserManager userManager;
-
 
     @Override
     public Users getCurrentUser() {
