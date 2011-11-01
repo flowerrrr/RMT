bda848def4216fc60922c15dd9aa4c0d0580dc67
flowerrrr <okarito2000@yahoo.de>
2011-11-01 02:19:48 +0100

    more work on player response page

--- rmt/src/main/java/de/flower/rmt/ui/common/form/FormFieldPanel.java
+++ rmt/src/main/java/de/flower/rmt/ui/common/form/FormFieldPanel.java
@@ -4,9 +4,7 @@ import org.apache.wicket.feedback.ComponentFeedbackMessageFilter;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.IMarkupFragment;
 import org.apache.wicket.markup.MarkupElement;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.FormComponent;
-import org.apache.wicket.markup.html.form.FormComponentLabel;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.PropertyModel;
@@ -17,8 +15,6 @@ import org.apache.wicket.model.ResourceModel;
  */
 public class FormFieldPanel extends Panel {
 
-    final Label labelSpan;
-
     final FormComponent formComponent;
 
     public FormFieldPanel(String id, FormComponent fc) {
@@ -27,11 +23,7 @@ public class FormFieldPanel extends Panel {
         setRenderBodyOnly(true);
 
         formComponent = fc;
-        final FormComponentLabel label = new FormComponentLabel("label", formComponent);
-        add(label);
-        labelSpan = new Label("labelSpan");
-        label.add(labelSpan);
-        label.add(formComponent);
+        add(formComponent);
         add(new FeedbackPanel("feedback", new ComponentFeedbackMessageFilter(formComponent)));
     }
 
@@ -44,7 +36,7 @@ public class FormFieldPanel extends Panel {
 
         // retrieving the 'calling' markup cannot be done in constructor.
         String labelKey = getLabelKey();
-        labelSpan.setDefaultModel(new ResourceModel(labelKey));
+        formComponent.setLabel(new ResourceModel(labelKey));
 
         // set model of form component
         formComponent.setModel(new PropertyModel(formComponent.getForm().getModel(), this.getId()));
