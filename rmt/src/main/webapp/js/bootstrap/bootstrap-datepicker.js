9c489f45bd9456d1ea2e2e9c5f33ebc877e44e0f
unknown <oblume@flower.(none)>
2012-04-02 15:32:35 +0200

    patch for date formatting

--- rmt/src/main/webapp/js/bootstrap/bootstrap-datepicker.js
+++ rmt/src/main/webapp/js/bootstrap/bootstrap-datepicker.js
@@ -336,3 +336,29 @@
   , startOfWeek: 1
   };
 }( window.jQuery || window.ender );
+
+// Patch for formatting
+/* Update datepicker plugin so that DD.MM.YYYY format is used. */
+$.extend($.fn.datepicker.defaults, {
+            parse: function (string) {
+                var matches;
+                if ((matches = string.match(/^(\d{2,2})\.(\d{2,2})\.(\d{4,4})$/))) {
+                    return new Date(matches[3], matches[2] - 1, matches[1]);
+                } else {
+                    return null;
+                }
+            },
+            format: function (date) {
+                var
+                        month = (date.getMonth() + 1).toString(),
+                        dom = date.getDate().toString();
+                if (month.length === 1) {
+                    month = "0" + month;
+                }
+                if (dom.length === 1) {
+                    dom = "0" + dom;
+                }
+                return dom + "." + month + "." + date.getFullYear();
+            }
+        });
+
