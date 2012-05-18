3c2bb277b16cfadf6b525c2cf0ceff636f98fb48
unknown <oblume@flower.(none)>
2012-05-18 11:28:18 +0200

    filtering supports '*' wildcard now

--- rmt/src/main/webapp/js/search-filter.js
+++ rmt/src/main/webapp/js/search-filter.js
@@ -1,11 +1,28 @@
+/* ====================================================================
+ * Script to filter dom.elements based in wildcard expressions.
+ * - add input field with id="filter"
+ * - mark list-items are table-rows with class="filterable"
+ * - mark content to be matched with class="filtered"
+ *
+ * e.g.
+ * <table>
+ *     <tr class="filtered">
+ *         <td class="filtered">First name / last name</td>
+ *         <td>[Some other text that should not be matched]</td>
+ *     </tr>
+ * </table>
+ * ==================================================================== */
+
 jQuery(document).ready(function($) {
     $.extend($.expr[":"], {
         "matches": function(elem, i, match, array) {
-            var query = (match[3] || "").toLowerCase();
-            var text = $(elem).find('.filtered').text();
-            var hit = (text || "").toLowerCase().indexOf(query);
+            var query = (match[3] || "");
+            query = query.replace('*', '.*');  // support simple '*' wildcard.
+            var regexp = new RegExp(query, 'i');
+            var text = ($(elem).find('.filtered').text() || "");
+            var hit = regexp.test(text);
             // console.log("hit=" + hit);
-            return hit >= 0;
+            return hit;
         }
     });
     
