21b3197bfab2ee84dcd850814b14a18113445324
unknown <oblume@flower.(none)>
2012-05-24 00:59:56 +0200

    added "password forgotten" link to login page

--- rmt/src/main/java/de/flower/rmt/ui/page/login/LoginPage.java
+++ rmt/src/main/java/de/flower/rmt/ui/page/login/LoginPage.java
@@ -2,13 +2,26 @@ package de.flower.rmt.ui.page.login;
 
 import de.flower.rmt.ui.page.base.AbstractBasePage;
 import de.flower.rmt.ui.page.base.AnonymousNavigationPanel;
+import org.apache.commons.lang.math.RandomUtils;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
  * @author flowerrrr
  */
 public class LoginPage extends AbstractBasePage {
+
+    private static String[] links = new String[]{
+            "http://www.buerlecithin.de/",
+            "http://www.dr-poehlmann.de/poehlmann/taiginseng/",
+            // "http://www.gedaechtnistraining.net/",
+            // "http://www.oekotest.de/cgi/index.cgi?artnr=38460;bernr=06;co=",
+            // "http://www.lizzynet.de/wws/32371748.php",
+            "http://keepass.info/",
+    };
 
     public LoginPage(final PageParameters params) {
 
@@ -20,5 +33,16 @@ public class LoginPage extends AbstractBasePage {
                 return !params.get("error").isNull();
             }
         });
+
+        add(new ExternalLink("passwordForgottenLink", getLinkModel()));
+    }
+
+    private IModel<String> getLinkModel() {
+        return new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                return links[RandomUtils.nextInt(links.length)];
+            }
+        };
     }
 }
