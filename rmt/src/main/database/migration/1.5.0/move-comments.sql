36c59a1c92902b28d0904ea8bb2222c0997eb620
flowerrrr <oliver@flower.de>
2012-06-16 18:39:20 +0200

    updated activity manager to new comment feature

--- rmt/src/main/database/migration/1.5.0/move-comments.sql
+++ rmt/src/main/database/migration/1.5.0/move-comments.sql
@@ -5,7 +5,15 @@ insert into comment (invitation_id, author_id, createDate, text)
         select id, user_id, date, comment from invitation where comment is not null;
 
 
+; h2
 insert into comment (invitation_id, author_id, createDate, text)
         select i.id, createdby_id, dateadd('mi', 1, date), managercomment
   from invitation i left join event e on (i.event_id = e.id)
   where managercomment is not null;
+
+; mysql
+insert into comment (invitation_id, author_id, createDate, text)
+        select i.id, createdby_id, date_add( date, INTERVAL 1 MINUTE ), managercomment
+  from invitation i left join event e on (i.event_id = e.id)
+  where managercomment is not null;
+
